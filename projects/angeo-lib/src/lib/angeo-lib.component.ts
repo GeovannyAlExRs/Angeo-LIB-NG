b782a8bf632a3c3ff8077fd2ef29b8426cf2f2d7
Alexander <axel94bsc@gmail.com>
2024-05-21 12:33:45 -0500

    Feature: config package to library

--- projects/angeo-lib/src/lib/angeo-lib.component.ts
+++ projects/angeo-lib/src/lib/angeo-lib.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'angeo-angeo-lib',
-  template: `
-    <p>
-      angeo-lib works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: 'angeo-lib',
+  template: ` <p>angeo-lib works!</p> `,
+  styles: [],
 })
-export class AngeoLibComponent {
-
-}
+export class AngeoLibComponent {}
